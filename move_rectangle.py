a94841f44f05ac89461e0d371f12ad02b6176c91
Aadarsh Nagrath <92307537+aadarsh-nagrath@users.noreply.github.com>
2024-11-09 20:54:18 +0530

    Create move_rectangle.py
    Simple customized code for moving turtle in a rectangle

---
+++ move_rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from time import sleep
+
+def move_rectangle():
+    rospy.init_node('move_rectangle', anonymous=True)
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    rate = rospy.Rate(1)
+
+    move_cmd = Twist()
+    linear_speed = 1.5  
+    angular_speed = 1.57
+
+    for _ in range(2):
+        move_cmd.linear.x = linear_speed
+        move_cmd.angular.z = 0.0
+        pub.publish(move_cmd)
+        sleep(1.5) 
+
+        move_cmd.linear.x = 0.0
+        move_cmd.angular.z = angular_speed
+        pub.publish(move_cmd)
+        sleep(0.75) 
+
+        move_cmd.linear.x = linear_speed
+        move_cmd.angular.z = 0.0
+        pub.publish(move_cmd)
+        sleep(1.5)
+
+        move_cmd.linear.x = 0.0
+        move_cmd.angular.z = angular_speed
+        pub.publish(move_cmd)
+        sleep(1.5)
+
+        move_cmd.linear.x = linear_speed + 3
+        move_cmd.angular.z = 0.0
+        pub.publish(move_cmd)
+        sleep(1.5)
+
+    move_cmd.linear.x = 0.0
+    move_cmd.angular.z = 0.0
+    pub.publish(move_cmd)
+    rospy.loginfo("Finished moving in a smaller rectangle")
+
+if __name__ == '__main__':
+    try:
+        move_rectangle()
+    except rospy.ROSInterruptException:
+        pass
